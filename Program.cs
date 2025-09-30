cadfa3e04dd9224a96c04fda0b001da6ca50a121
RedginaldGodeau <redginaldgodeau95@gmail.com>
2025-09-30 11:26:46 +0200

    feat: Adding scene and create gamescene

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 using System.Numerics;
 using Raylib_cs;
 using SnakeGame.entity;
+using SnakeGame.scene;
 
 namespace SnakeGame
 {
@@ -11,52 +12,24 @@ namespace SnakeGame
         static readonly int WindowWidth = 1024;
         static readonly int WindowHeight = 768;
         
-        
         static void Main()
         {
-            Random rand = new Random();
-            
             Raylib.InitWindow(WindowWidth, WindowHeight, "Snake Game");
             Raylib.SetTargetFPS(60);
-
-            int score = 0;
             
-            Collider groundCollider = new Collider(Vector2.Zero, new Vector2(WindowWidth, WindowHeight));
+            GameScene gameScene = new GameScene(WindowWidth, WindowHeight);
             
-            Player player = new Player(new Vector2(0, 0), new Vector2(50, 50), 100, 0);
-            Food food = new Food(new Vector2(rand.Next(0, WindowWidth), rand.Next(0, WindowHeight)),
-                new Vector2(30, 30));
 
             while (!Raylib.WindowShouldClose())
             {
                 // UPDATE
                 float deltaTIme = Raylib.GetFrameTime();
                 
-                player.Update(deltaTIme);
-
-                if (player.TouchFood(food.GetCollider()))
-                {
-                    score++;
-                    player.AddSnakeTail(1);
-                    food.SetPosition(new Vector2(rand.Next(0, WindowWidth), rand.Next(0, WindowHeight)));
-                }
-
-                if (player.TouchTail() || !groundCollider.CheckCollide(player.GetCollider()))
-                {
-                    Raylib.CloseWindow();
-                }
+                gameScene.Update(deltaTIme);
                 
                 // DRAW
                 Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.White);
-
-                player.Draw();
-                food.Draw();
-                
-                
-                // UI
-                Raylib.DrawText("Score : " + score, 0, 0, 50, Color.Beige);
-                
+                gameScene.Draw();
                 Raylib.EndDrawing();
             }
         }
---
+++ src/scene/GameScene.cs
@@ -0,0 +1,48 @@
+using System.Numerics;
+using SnakeGame.entity;
+using Raylib_cs;
+
+namespace SnakeGame.scene;
+
+public class GameScene(int width, int height)
+{
+    private static readonly Random Rand = new Random();
+    
+    private int _score = 0;
+    private readonly Collider _groundCollider = new Collider(Vector2.Zero, new Vector2(width, height));
+    
+    private readonly Player _player = new Player(new Vector2(0, 0), new Vector2(50, 50), 100, 0);
+    private readonly Food _food = new Food(new Vector2(Rand.Next(0, width), Rand.Next(0, height)),
+        new Vector2(30, 30));
+
+    public int GetScore() => this._score;
+
+    public void Update(float deltaTime)
+    {
+        _player.Update(deltaTime);
+
+        if (_player.TouchFood(_food.GetCollider()))
+        {
+            _score++;
+            _player.AddSnakeTail(1);
+            _food.SetPosition(new Vector2(Rand.Next(0, width), Rand.Next(0, height)));
+        }
+
+        if (_player.TouchTail() || !_groundCollider.CheckCollide(_player.GetCollider()))
+        {
+            Raylib.CloseWindow();
+        }
+    }
+
+    public void Draw()
+    {
+        Raylib.ClearBackground(Color.White);
+
+        _player.Draw(); 
+        _food.Draw();
+        
+        // UI
+        Raylib.DrawText("Score : " + _score, 0, 0, 50, Color.Beige);
+    }
+    
+}
